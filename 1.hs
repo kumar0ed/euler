1b46c8be6f8435ad8134cd2fb8e821081ffadc29
kumar edward <kum.ar0ed+github@gmail.com>
2017-08-23 17:14:16 +0530

    problem 1 idea 1

--- 1.hs
+++ 1.hs
@@ -1,2 +1,8 @@
---idea 0
-main=print$ sum [x|x<-[1..999],x`mod`3==0||x`mod`5==0]
+--idea 1
+sumOfN n=n*(n+1)`div`2
+sumOfMultiplesOfN m n=sumOfN$ m`div`n
+main=print$ 3*(sumOfMultiplesOfN 999 3)+5*(sumOfMultiplesOfN 999 5)-15*(sumOfMultiplesOfN 999 15)
+
+
+----idea 0
+--main=print$ sum [x|x<-[1..999],x`mod`3==0||x`mod`5==0]
