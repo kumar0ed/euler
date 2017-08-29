ebd0f50b028b310108b52553240578f84a3744ba
kumar edward <kum.ar0ed+github@gmail.com>
2017-08-29 17:16:04 +0530

    problem 7 idea 0

--- 7.hs
+++ 7.hs
@@ -1,5 +1,18 @@
 {-# OPTIONS_GHC -O3 -optc-O3 #-}
 
-wheel n=map (m+) [1, 7, 11, 13, 17, 19, 23, 29]
+wheelList =  [1, 7, 11, 13, 17, 19, 23, 29]
+
+wheel n=map (m+) wheelList
   where m=30*n
+
+candidates =concatMap wheel [1..]
+
+isPrime n=not.(any isDivicible)$ rootCandidates
+  where
+    rootCandidates =takeWhile (<=(ceiling.sqrt.fromIntegral$ n)) primes
+    isDivicible x= n`mod`x==0
+
+primes = [2, 3, 5]++(tail wheelList)++(filter isPrime candidates)
+
+main =print$ primes!!(10001-1)
 
